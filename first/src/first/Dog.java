6cf8a2b89c8455b3b49625b37a1f4bd66d80fd3c
heyeping <942060752@qq.com>
2014-08-27 15:13:13 +0800

    .update code

---
+++ first/src/first/Dog.java
@@ -0,0 +1,10 @@
+package first;
+
+public class Dog {
+   String bread;
+   int age;
+   String color;
+   void barking(){}
+   void sleeping(){}
+   void hungry(){}
+}
---
+++ first/src/first/FreshJuiceTest.java
@@ -0,0 +1,13 @@
+package first;
+
+class FreshJuice{
+	enum FreshJuiceSize{SMALL,MEDUIM,LARGE}
+	FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+    public static void main(String args[]){
+    	FreshJuice juice = new FreshJuice();
+    	juice.size =FreshJuice.FreshJuiceSize.MEDUIM;
+    }
+}

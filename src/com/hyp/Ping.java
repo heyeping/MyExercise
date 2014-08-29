88f22ed5f0e93a8220ecf38bf6beaa696fe7c3f4
heyeping <942060752@qq.com>
2014-08-29 12:11:56 +0800

    .use string

---
+++ src/com/hyp/Ping.java
@@ -0,0 +1,12 @@
+package com.hyp;
+
+public class Ping {
+
+	public static void main(String[] args) {
+
+		StringBuffer str = new StringBuffer();
+		str.append("1").append(true).append(5);
+		System.out.println(str.toString());
+	}
+
+}
